a2685bb645fef489df2c6682f92f418985fa4a2d
Gleb1371 <163561026+Gleb1371@users.noreply.github.com>
2024-05-24 00:11:34 +0700

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,4 +1,4 @@
-function postclasses(event) {
+function clientpost() {
     var phone = document.getElementById("phone").value;
     var first_name = document.getElementById("name").value;
     var zapros = new XMLHttpRequest();
@@ -47,10 +47,22 @@ function postclasses(event) {
         }
     }
 }
+function showError(message) {
+    errorAlert.textContent = message;
+    errorAlert.classList.add('active');
+    successAlert.textContent = "";
+    successAlert.classList.remove('active');
+}
+
+function showSuccess(message) {
+    successAlert.textContent = message;
+    successAlert.classList.add('active');
+    errorAlert.textContent = "";
+    errorAlert.classList.remove('active');
+}
 
 
-
-function posttrips() {
+function post1() {
     var zapros = new XMLHttpRequest();
     var modalBtn = document.getElementById("modalBtn");
     var modal = document.getElementById("myModal");
@@ -79,17 +91,17 @@ function posttrips() {
         if (phone.length == 11 && first_name > '') {
             zapros.onload = function () {
                 if (zapros.status === 200) {
-                    showSuccess("Спасибо,мы вам перезвоним!");
+                    showSuccesss("Спасибо,мы вам перезвоним!");
                 } else {
                     try {
                         var errorResponse = JSON.parse(zapros.responseText);
                         if (errorResponse.error) {
                             alert("Ошибка: " + errorResponse.error);
                         } else {
-                            showError("Такие данные уже существуют.");
+                            showErrorr("Такие данные уже существуют.");
                         }
                     } catch (error) {
-                        showError("Произошла ошибка. Пожалуйста, попробуйте еще раз.");
+                        showErrorr("Произошла ошибка. Пожалуйста, попробуйте еще раз.");
                     }
                 }
             };
@@ -101,14 +113,14 @@ function posttrips() {
         }
         else {
             if (first_name > '' && phone.length > 0) {
-                showError("Некорректный номер телефона!");
+                showErrorr("Некорректный номер телефона!");
             }
             else if (first_name > '' && phone.length == "") {
-                showError("Пожалуйста, введите номер телефона!");
+                showErrorr("Пожалуйста, введите номер телефона!");
             }
 
             else if (phone.length == 11 && first_name == 0) {
-                showError("Пожалуйста, введите имя!");
+                showErrorr("Пожалуйста, введите имя!");
             }
             else {
                 showError("Пожалуйста, заполните поля!");
@@ -119,32 +131,20 @@ function posttrips() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    posttrips(); // Вызываем функцию для инициализации обработчиков событий
+    post1(); // Вызываем функцию для инициализации обработчиков событий
 });
 
 
 
-function showError(message) {
-    errorAlert.textContent = message;
-    errorAlert.classList.add('active');
-    successAlert.textContent = "";
-    successAlert.classList.remove('active');
-}
 
-function showSuccess(message) {
-    successAlert.textContent = message;
-    successAlert.classList.add('active');
-    errorAlert.textContent = "";
-    errorAlert.classList.remove('active');
-}
-function showError(message) {
+function showErrorr(message) {
     errorAlert1.textContent = message;
     errorAlert1.classList.add('active');
     successAlert1.textContent = "";
     successAlert1.classList.remove('active');
 }
 
-function showSuccess(message) {
+function showSuccesss(message) {
     successAlert1.textContent = message;
     successAlert1.classList.add('active');
     errorAlert1.textContent = "";
